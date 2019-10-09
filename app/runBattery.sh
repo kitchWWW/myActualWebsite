6583ca4ac64e61677458b45aaf0cc8cbe8e82e6f
kitchWWW <brian.e2014@gmail.com>
2019-10-08 21:57:19 -0700

    meg battery alabama
    Former-commit-id: 4f484f15f27aaa9a13300e8884852f1a170b89b6
    Former-commit-id: fb910a6e7133faf2827c8b9a7eaa34ef4fcc3d70

---
+++ app/runBattery.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Starting from Battery Server Call"
+echo $1 $2 $3 $4
+cd engines/BatteryLow-master
+python make.py $1 $2 $3 $4
+echo "Completed run"
+cd out/$1
+/Applications/Lilypond.app/Contents/Resources/bin/lilypond score.ly
+lilypond $score.ly
